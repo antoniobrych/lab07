9f31c6fd57b8580c2894e0601ef4601ed1552bb3
Antonio Brych <63557494+antoniobrych@users.noreply.github.com>
2025-04-08 12:51:53 -0300

    Add files via upload

---
+++ src/lib/Project.svelte
@@ -0,0 +1,29 @@
+<script>
+    export let data = {};
+    export let hLevel = 2;
+</script>
+
+<article>
+    <svelte:element this={"h" + hLevel}>{data.title}</svelte:element>
+    <img src={data.image} alt="" />
+    <div>
+        <p>
+            {data.description}
+        </p>
+        <p class="year">
+            {data.year}
+        </p>
+    </div>
+</article>
+
+<style>
+    p {
+        font-size: 0.7em;
+    }
+    img {
+        height: 12em;
+    }
+    p.year{
+        font-size: 1.5em;
+    }
+</style>
